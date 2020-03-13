24bb1c4fba2e39ffa19a05855a7c31e90fd9dada
harshdeep3 <harshdeep366@gmail.com>
2020-03-13 10:38:42 +0000

    Editing the files, trying to get the ranking to work

--- src/Component/Post/PostList.js
+++ src/Component/Post/PostList.js
@@ -9,6 +9,7 @@ import PostData5 from '../../Data/article-5.json';
 
 // sends the files to the component
 import PostDetail from './PostDetail';
+import Ranking from './Ranking';
 // css
 import '../../css/PostListCSS.scss'
 
@@ -18,6 +19,7 @@ function PostList(props) {
   const [currentPage, setCurrentPage] = useState(0);
   const [articles, setArticles] = useState([]);
   const [lastArticle, setLastArticles] = useState(false)
+  const [rating, setRating] = useState("");
 
   // adds the artciles to a dictionary
   useEffect(() => {
@@ -38,7 +40,6 @@ function PostList(props) {
     );
   } else {
     return (
-
       <div className="articles container-fluid">
         {/* using bootstrap to disign the page. Add margin to the side the articles are readable */}
         <div className="row">
@@ -64,7 +65,7 @@ function PostList(props) {
                 // allows the user to go back to the pervious article
               }
               else{
-                alert("No more pages!!!!!!!!!!!");
+                alert("No more pages! Go forward instead.");
                 // if the user gets to the first page and try going further, the user will be informed that there arent any more pages
               }
             }} type="button" className="btn btn-primary" id="previousPage">Go Previous Page</button>
@@ -74,10 +75,13 @@ function PostList(props) {
               if (currentPage < articles.length - 1) {
                 setCurrentPage(currentPage + 1);
                 // This is the event to go to the next pages
-              } else {
-                setLastArticles(true);
-              }
+                if(currentPage == articles.length - 1){
+                  setLastArticles(true);
+                }
+
+              } 
             }} type="button" className="btn btn-primary" id="nextPage">Next Page</button>
+          ``
           </div>
           <div className="col-lg-1 col-md-1 col-sm-2"> </div>
         </div>
